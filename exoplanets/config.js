0c249357814cf22e55e2dd9fa9ff74e1c94d1d46
laurent vdm <laurent.vandermeersch@student.vinci.be>
2022-11-28 11:56:35 +0100

    not hardcoded config.js

--- exoplanets/config.js
+++ exoplanets/config.js
@@ -1,5 +1,5 @@
-const DATABASE_URL = 'postgres://mlqlchcy:gBqOTIcGhuZ06Y73F83byA7IQ6avsuWS@lucky.db.elephantsql.com/mlqlchcy'
+
 
 module.exports = {
-    DATABASE_URL
+    ConnectionString: process.env.DATABASE_URL
 };
